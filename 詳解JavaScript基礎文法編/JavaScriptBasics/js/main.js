f1430ad6e75d3ad06d6be62692c71e0c2d7e0dff
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-19 17:25:04 +0900

    Add files via upload
    #23

--- 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
+++ 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
@@ -1,12 +1,11 @@
 'use strict'
 
-const sum = function sum(a, b, c){
-  return a + b + c;
-};
+// const sum = (a, b, c) => a + b + c;
+//
+// const total = sum(1,2,3) + sum(3,4,5);
+//
+// console.log(total);
 
-// sum(1,2,3);
-// sum(3,4,5);
+const double = a => a*2;
 
-const total = sum(1,2,3) + sum(3,4,5);
-
-console.log(total);
+console.log(double(12));
