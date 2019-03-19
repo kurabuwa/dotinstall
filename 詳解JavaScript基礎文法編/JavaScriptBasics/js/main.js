5be8aedcb4ee2760dcac1ca3a9847b6874a30039
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-19 16:34:30 +0900

    Add files via upload
    #11

--- 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
+++ 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
@@ -1,7 +1,5 @@
   'use strict';
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+console.log('5' * 3);
+console.log('5' + 3);
+console.log(parseInt('hello',10));
