1026f76dba5bcd01e63a32fb6f5869acbfb4a656
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-19 17:01:34 +0900

    Add files via upload
    #17

--- 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
+++ 詳解JavaScript基礎文法編/JavaScriptBasics/js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
-for(let i = 1; i<=10; i++){
-  // console.log('hello');
-  console.log(i + 'hello');
-  console.log(`${i}hello`)
+let hp=200;
+
+// while(hp > 0){
+//   console.log(`${hp} hp left!`);
+//   hp -= 15;
+// }
+
+for (let hp = 200; hp>0; hp-=15){
+  console.log(`${hp} hp left!`)
 }
