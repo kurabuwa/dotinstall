e446206f40c8a4108a27841ab840451287e3a464
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-20 18:40:34 +0900

    Add files via upload
    #3

--- 詳解JavaScript DOM編/JavaScriptBasics/js/main.js
+++ 詳解JavaScript DOM編/JavaScriptBasics/js/main.js
@@ -1,4 +1,7 @@
 'use strict';
 {
-  document.getElementById('target').textContent = 'Changed!';
+  document.querySelector('h1').textContent='H1!';
+  document.querySelectorAll('li:nth-child(odd)').forEach(li => {
+    li.textContent = 'li!';
+  });
 }
