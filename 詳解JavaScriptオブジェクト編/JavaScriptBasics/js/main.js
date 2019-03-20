5eb779f1c05c5377ae6af5cdfa96bb960af19b90
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-20 16:36:18 +0900

    Add files via upload
    #16

--- 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
+++ 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
@@ -1,21 +1,8 @@
 'use strict';
 {
-  // const numbers=[1,2,3,8];
-  // const [a,b,...rest]=numbers;
-  // console.log(a);
-  // console.log(b);
-  // console.log(rest);
+  const str = 'hello';
 
-  const player = {
-    name: 'taguchi',
-    score: 55,
-    hp: 33,
-    mp: 22,
-  };
-
-  const {name, score ,...point}=player;
-  console.log(name);
-  console.log(score);
-  console.log(point);
-
+  console.log(str.length);
+  console.log(str.substring(2, 4));
+  console.log(str[1]);
 }
