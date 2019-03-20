b4fe03c3c4864e8aeaa38ef42a471c37d7b898ea
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-20 16:49:39 +0900

    Add files via upload
    #19

--- 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
+++ 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 {
-  const d = new Date();
-  console.log(d);
-  console.log(d.getMonth());
-  console.log(d.getDate());
-  console.log(d.getDay());
-  console.log(d.getHours());
-  console.log(d.getMinutes());
-  console.log(d.getSeconds());
-  console.log(d.getTime());
-
+//   const d = new Date(2018, 11);
+// d.setHours(10,20,30);
+// d.setDate(32);
+// d.setDate(d.getDate() +3);
+//
+//
+//   console.log(d);
+  const d1 = new Date(2018, 11,1);
+  const d2 = new Date(2018,11,10);
+  console.log((d2 - d1)/(24 * 60 * 60 *1000));
 }
