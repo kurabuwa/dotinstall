54fa07aef0e162f016b6895328a2e5b13cbc7f1a
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-20 16:10:27 +0900

    Add files via upload
    #12

--- 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
+++ 詳解JavaScriptオブジェクト編/JavaScriptBasics/js/main.js
@@ -1,10 +1,12 @@
 'use strict';
 {
   const a = [1, 5, 10];
-  a.splice(2,0,6,7);
-  console.log(a);
 
-  const removed = a.splice(1,2);
-  console.log(a);
-  console.log(removed);
+  // for (let i=0; i < a.length ; i++){
+  //   console.log(a[i]);
+  // }
+
+  a.forEach((item, index) => {
+    console.log(`${index}:${item}`);
+  })
 }
