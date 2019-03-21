cd6be751deb14a570554f1534fcd1c914b096d90
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-21 16:53:36 +0900

    Add files via upload
    #8

--- JavaScriptでおみくじを作ろう/js/main.js
+++ JavaScriptでおみくじを作ろう/js/main.js
@@ -4,20 +4,19 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () =>{
-    const result = ['大吉', '中吉', '凶','末吉']
-    const n = Math.floor(Math.random() * result.length)
-    btn.textContent = result[n];
-    // switch (n){
-    //   case 0:
-    //     btn.textContent = '大吉';
-    //     break;
-    //   case 1:
-    //     btn.textContent= '中吉';
-    //     break;
-    //   case 2:
-    //     btn.textContent= '凶';
-    //     break;
-    // }
+    // const result = ['大吉', '中吉', '凶','末吉']
+    // const result = ['大吉', '大吉', '凶','大吉']
+    // const n = Math.floor(Math.random() * result.length)
+    // btn.textContent = result[n];
+    const n = Math.random();
+    if(n<0.05){
+      btn.textContent = '大吉';
+    }else if(n<0.2){
+      btn.textContent = '中吉';
+    }else{
+      btn.textContent = '凶';
+    }
+
   })
   btn.addEventListener('mousedown', () =>{
     btn.classList.add('pressed');
