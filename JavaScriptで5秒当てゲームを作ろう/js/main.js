44e8b5742e88042afb3ef8449c3c4085383e1854
kurabuwa <mamikurabuwa831@yahoo.co.jp>
2019-03-21 17:25:40 +0900

    Add files via upload
    #5

--- JavaScriptで5秒当てゲームを作ろう/js/main.js
+++ JavaScriptで5秒当てゲームを作ろう/js/main.js
@@ -4,14 +4,17 @@
   var start = document.getElementById('start');
   var stop = document.getElementById('stop');
   var result = document.getElementById('result');
-  var starTime;
+  var startTime;
 
-  start.addEventLitener('click', function(){
-    startTime = Date.now;
+  start.addEventListener('click', function(){
+    startTime = Date.now();
   });
 
-  start.addEventLitener('click', function(){
-
+  stop.addEventListener('click', function(){
+    var elapsedTime;
+    // elapsedTime = (Date.now() - startTime)/1000;
+    // elapsedTime = 4;
+    result.textContent=elapsedTime.toFixed(3);
   })
 
-})
+})();
